94e6995e2530571c75379ad8908c88c291775ce6
soorq <fantastfad830@gmail.com>
2024-12-09 00:18:37 +0300

    refactor: seo meta tags and package.json#4

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,74 @@
 import type { Metadata } from 'next';
 import dynamic from 'next/dynamic';
 
+const $app = process.env.NEXT_PUBLIC_APP_URL;
+
 // TreeShaking activating
 const RootPage = dynamic(() => import('@/screens/root').then(p => p.RootPage), { ssr: false });
 
 // Seo meta tags for page
 export const metadata: Metadata = {
-	title: 'Root Page',
-	description: 'Example dashboard app built using the components.',
+	metadataBase: new URL($app),
+	title: 'ТОРЭЛЕКТРО | Профессиональные удлинители и аксессуары',
+	description:
+		'Профессиональные удлинители высокого качества для любых нужд. Широкий ассортимент продукции, надежные материалы, безопасное использование. Узнайте больше на нашем сайте!',
+	keywords: ['Москва', 'Московская область', 'удлинители', 'Удлинители'],
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			index: true,
+			follow: true,
+			'max-video-preview': -1,
+			'max-image-preview': 'large',
+			'max-snippet': -1,
+		},
+	},
+	openGraph: {
+		title: 'ТОРЭЛЕКТРО',
+		description:
+			'Профессиональные удлинители высокого качества для любых нужд. Широкий ассортимент продукции, надежные материалы, безопасное использование. Узнайте больше на нашем сайте!',
+		type: 'website',
+		locale: 'ru-RU',
+		url: new URL($app),
+		siteName: 'ТОРЭЛЕКТРО',
+		images: [
+			{
+				url: '/meta/og.png',
+				alt: 'ТОРЕЛЕКТРО логотип',
+			},
+		],
+	},
+	alternates: {
+		canonical: new URL($app),
+	},
+	referrer: 'no-referrer-when-downgrade',
+	twitter: {
+		card: 'summary',
+		images: [
+			{
+				url: '/meta/twitter.png',
+				alt: 'ТОРЕЛЕКТРО логотип',
+			},
+		],
+		title: '',
+		description:
+			'Профессиональные удлинители высокого качества для любых нужд. Широкий ассортимент продукции, надежные материалы, безопасное использование. Узнайте больше на нашем сайте!',
+		site: new URL($app).toString(),
+	},
+	authors: [
+		{
+			name: 'Soorq',
+			url: 'https://github.com/soorq',
+		},
+		{
+			name: 'Ivan41kd',
+			url: 'https://github.com/ivan41kd',
+		},
+	],
+	verification: {
+		yandex: 'your-yandex-verification-code',
+	},
 };
 
 export default RootPage;
--- cypress/components/root.cy.tsx
+++ cypress/components/root.cy.tsx
@@ -1,14 +1,12 @@
-import RootPage from '../../app/page';
+import RootPage from '../../src/screens/root';
 
-describe('<Page />', () => {
+describe('<RootPage />', () => {
 	it('should render and display expected content', () => {
 		cy.mount(<RootPage />);
 
-		// The new page should contain an h1 with "Home"
-		cy.get('h1').contains('Home');
-
-		// Validate that a link with the expected URL is present
-		// Following the link is better suited to an E2E test
+		cy.get('h1').contains(
+			'Российские удлинители собственного производства <br /> по международным стандартам',
+		);
 		cy.get('a[href="/about"]').should('be.visible');
 	});
 });
